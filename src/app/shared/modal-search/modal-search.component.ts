2fdf2aaa8710c00601f6ae017fb461080fae6ddf
Ronal Adrian Ruiz Avila <ronalruizprin@gmail.com>
2019-09-29 16:00:04 -0500

    push commit

--- src/app/shared/modal-search/modal-search.component.ts
+++ src/app/shared/modal-search/modal-search.component.ts
@@ -5,42 +5,45 @@ import {Route} from '@angular/router';
 
 
 @Component({
-  selector: 'app-modal-search',
-  templateUrl: './modal-search.component.html',
-  styleUrls: ['./modal-search.component.css'],
-  providers: [
-    SpeechRecognitionService,
-  ],
+    selector: 'app-modal-search',
+    templateUrl: './modal-search.component.html',
+    styleUrls: ['./modal-search.component.css'],
+    providers: [
+        SpeechRecognitionService,
+    ],
 })
 export class ModalSearchComponent implements OnInit {
-  private synth = window.speechSynthesis;
-  message = '';
-  private voices;
+    private synth = window.speechSynthesis;
+    message = '';
+    private voices;
+    listen = ""
 
-  constructor(public _speech: SpeechRecognitionService, public dialogRef: MatDialogRef<ModalSearchComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {
-  }
-
-  ngOnInit() {
-  }
-
-  commandVoice() {
-    if (this.synth !== null) {
-      this.voices = this.synth.getVoices();
-      const utterThis = new SpeechSynthesisUtterance('Que pelicula desea Buscar');
-      utterThis.lang = 'es-EC';
-      this.synth.speak(utterThis);
-      const speking = this.synth.speaking;
-      setTimeout(() => {
-        this._speech.start();
-      }, 2000);
-      this._speech.onresult = ({results}) => {
-        this.message = results.item(0).item(0).transcript;
-      };
-      this._speech.onend = () => {
-        this.dialogRef.close({name: this.message});
-      };
+    constructor(public _speech: SpeechRecognitionService, public dialogRef: MatDialogRef<ModalSearchComponent>,
+                @Inject(MAT_DIALOG_DATA) public data: any) {
     }
-  }
+
+    ngOnInit() {
+    }
+
+    commandVoice() {
+        if (this.synth !== null) {
+            this.voices = this.synth.getVoices();
+            const utterThis = new SpeechSynthesisUtterance('Que pelicula desea Buscar');
+            utterThis.lang = 'es-EC';
+            this.synth.speak(utterThis);
+            const speking = this.synth.speaking;
+            this.listen = 'warn';
+            setTimeout(() => {
+                this._speech.start();
+            }, 2000);
+            this._speech.onresult = ({results}) => {
+                this.message = results.item(0).item(0).transcript;
+            };
+            this._speech.onend = () => {
+                this.listen = '';
+                this.dialogRef.close({name: this.message});
+            };
+        }
+    }
 
 }
